d091d814bfa15286608c9466f1ce8565403fd72c
Florian Barth <barth@stocard.de>
2015-08-17 19:34:50 +0200

    Cleaning up after install echo versions of casper and phantom

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
-npm install -g casperjs
 apt-get update -qq
 apt-get install -y -qq libsqlite3-dev libfontconfig1-dev libicu-dev libfreetype6 libssl-dev \
       libpng-dev libjpeg-dev python libx11-dev libxext-dev
+
+npm install -g casperjs
+
+apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+casperjs --version
+
+phantomjs --version
